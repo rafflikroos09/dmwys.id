ddc7d6d683483661d5b0df3121709d1db9a77d97
budi@gmail.com <mhmd.raffli09@gmail.com>
2024-02-07 15:31:24 +0700

    push folder day3, push javascript for contact from

---
+++ day 2/script.js
@@ -0,0 +1,13 @@
+function getData() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("message").value;
+
+  console.log(name);
+  console.log(email);
+  console.log(phone);
+  console.log(subject);
+  console.log(message);
+}
