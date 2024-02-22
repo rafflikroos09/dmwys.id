beb6805beb834af2205374af6961988a5479c49f
budi@gmail.com <mhmd.raffli09@gmail.com>
2024-02-22 13:25:47 +0700

    service contact from

--- day 2/assets/js/script.js
+++ day 2/assets/js/script.js
@@ -1,11 +1,11 @@
 function getData() {
-  let name = document.getElementById("name").value;
+  let nama = document.getElementById("nama").value;
   let email = document.getElementById("email").value;
   let phone = document.getElementById("phone").value;
   let subject = document.getElementById("subject").value;
   let message = document.getElementById("message").value;
 
-  if ((name = "")) {
+  if (nama == "") {
     return alert("Please type your name");
   } else if (email == "") {
     return alert("Please type your email");
@@ -17,13 +17,19 @@ function getData() {
     return alert("Plese type your message");
   }
 
+  console.log(nama);
+  console.log(email);
+  console.log(phone);
+  console.log(subject);
+  console.log(message);
+
   const emailDestination = "hi.mhmd.raffli09@gmail.com";
   let a = document.createElement("a");
-  a.href = `mailto:${emailDestination}?subject=${subject}&body= halo bang nama saya, ${name}, saya ingin ${message}. bisakah anda menghubungi saya ${phone}`;
+  a.href = `mailto:${emailDestination}?subject=${subject}&body= halo bang nama saya, ${nama}, saya ingin ${message}. bisakah anda menghubungi saya ${phone}`;
   a.click();
 
   const data = {
-    name,
+    nama,
     email,
     phone,
     subject,
